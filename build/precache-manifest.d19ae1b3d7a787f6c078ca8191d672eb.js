53f109c1c8d6e0e4f525f665f734467428f9413d
J-a-r-e-k <23661244+J-a-r-e-k@users.noreply.github.com>
2019-04-28 21:01:51 +0200

    Build for github pages

---
+++ build/precache-manifest.d19ae1b3d7a787f6c078ca8191d672eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a776ac521b63f6cb47530751fa69fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37c067b1fd0ee984b785",
+    "url": "./static/css/main.b07a8eb2.chunk.css"
+  },
+  {
+    "revision": "5d34567fbe4002c81a05",
+    "url": "./static/js/2.79721b61.chunk.js"
+  },
+  {
+    "revision": "37c067b1fd0ee984b785",
+    "url": "./static/js/main.7832e8a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
