e63cce5078b1f8ec0e580c3e567d8584714e0da7
J-a-r-e-k <23661244+J-a-r-e-k@users.noreply.github.com>
2019-09-08 17:58:05 +0200

    bump dependencies

---
+++ build/precache-manifest.32da61584a4b33da247c14b667353716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772bc446afdbdbf2ea181fa509bf97f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0633b24fb1a852d50e4",
+    "url": "./static/css/main.927597e1.chunk.css"
+  },
+  {
+    "revision": "8c6e79a64f793d4b2299",
+    "url": "./static/js/2.908a529a.chunk.js"
+  },
+  {
+    "revision": "c0633b24fb1a852d50e4",
+    "url": "./static/js/main.7f9bd8a2.chunk.js"
+  },
+  {
+    "revision": "75d111bc0b8dd851e293",
+    "url": "./static/js/runtime~main.76093357.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.760e3fe2107b476cc38a3701225ecb66.js"
+  "./precache-manifest.32da61584a4b33da247c14b667353716.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
